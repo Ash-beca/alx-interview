5681ae28a2d4ddaff8c4f5c74fa9b60a16edb068
jemima <ashbeca16@gmail.com>
2024-03-14 13:31:52 +0200

    adding all files

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Lockboxes Puzzle Solution"""
+
+
+def canUnlockAll(boxes):
+    """Determines whether all the boxes can be unlocked"""
+    if not isinstance(boxes, list) or len(boxes) == 0:
+        return False
+
+    for key_to_check in range(1, len(boxes) - 1):
+        unlocked = False
+        for box_index in range(len(boxes)):
+            unlocked = (
+                key_to_check in boxes[box_index] and
+                key_to_check != box_index
+            )
+            if unlocked:
+                break
+        if not unlocked:
+            return unlocked
+
+    return True
