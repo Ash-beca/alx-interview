da911929232275ab355a1bd60d2c465799b0a643
jemima <ashbeca16@gmail.com>
2024-03-28 15:41:02 +0200

    adding all files

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,75 +1,51 @@
 #!/usr/bin/python3
-"""
-Module defines main function which outputs summar of logs
-"""
-import re
+
 import sys
 
 
-def main() -> None:
+def print_statistics(status_dict, total_file_size):
     """
-    Function infinitely loops reading from stdin and parse to output.
-    """
-    stat_cod_dict = {
-        '200': 0, '301': 0, '400': 0, '401': 0,
-        '403': 0, '404': 0, '405': 0, '500': 0,
-    }
-    lines_count, tot_file_size = 0, 0
-
-    try:
-        for line in sys.stdin:
-            if check_format(line):
-                file_size = int(line.split(' ')[-1])
-                stat_code = line.split(' ')[-2]
-                tot_file_size += file_size
-                stat_cod_dict[stat_code] += 1
-                lines_count += 1
-            if lines_count == 10:
-                output_summary(tot_file_size, stat_cod_dict)
-                lines_count = 0
-        output_summary(tot_file_size, stat_cod_dict)
-    except (KeyboardInterrupt):
-        output_summary(tot_file_size, stat_cod_dict)
-    return None
-
-
-def check_format(log: str) -> bool:
-    """
-    Verify single line log format
-    """
-    host_re = r'((\d{1,3}\.){3}\d{1,3}|\w+)\s?'
-    hyphen_re = r'\-\s?'
-    date_re = r'\[\d{4}\-\d{2}\-\d{2}\s(\d{2}:){2}\d{2}\.\d{6}\]\s'
-    request_re = r'"GET\s/projects/260\sHTTP/1.1"\s'
-    method_re = r'\d{3}\s'
-    file_size_re = r'\d+'
-
-    log_re = re.compile(
-        host_re + hyphen_re + date_re + request_re + method_re + file_size_re
-    )
-
-    if log_re.match(log):
-        return True
-    return False
-
-
-def output_summary(tot_size: int, status_codes: dict) -> None:
-    """Output to stdout summary of 10 log files
+    Print the statistics of the log file.
 
     Args:
-        tot_size (int): Total byte size from beginning
-        status_codes (dict): Status codes count dictionary
+        status_dict (dict): A dictionary containing the count
+        of each status code.
+        file_size (int): The total file size.
 
     Returns:
         None
     """
-    sys.stdout.write('File size: {}\n'.format(tot_size))
-    for code, count in sorted(status_codes.items()):
-        if status_codes.get(code):
-            sys.stdout.write('{}: {}\n'.format(code, count))
-    sys.stdout.flush()
-    return None
+    print("File size:", total_file_size)
+    for code, count in sorted(status_dict.items()):
+        if count != 0:
+            print("{}: {}".format(code, count))
 
 
-if __name__ == '__main__':
-    main()
+total_file_size = 0
+counter = 0
+status_dict = {
+    "200": 0, "301": 0, "400": 0, "401": 0,
+    "403": 0, "404": 0, "405": 0, "500": 0
+}
+
+try:
+    for line in sys.stdin:
+        parsed_line = line.strip().split()
+
+        counter += 1
+
+        # Extract file size and status code
+        file_size = int(parsed_line[-1])
+        # Second last element is status code
+        status_code = parsed_line[-2]
+
+        if status_code in status_dict:
+            total_file_size += file_size
+            status_dict[status_code] += 1
+
+        if counter == 10:
+            print_statistics(status_dict, total_file_size)
+            counter = 0
+
+finally:
+    print_statistics(status_dict, total_file_size)
